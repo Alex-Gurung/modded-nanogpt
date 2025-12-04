f56dc2d29f6843904f8bf3e9d7a93b95c952e0f6
Alex Gurung <aag1234@gmail.com>
2025-12-04 20:12:21 +0000

    dump

--- popo.py
+++ popo.py
@@ -1343,7 +1343,8 @@ optimizer1 = DistAdam(
     eps=1e-8,
     weight_decay=0.0,
 )
-optimizer2 = NorMuon(hidden_matrix_params + gate_params, lr=0.03, momentum=0.95, beta2=0.95, weight_decay=1.2)
+# optimizer2 = NorMuon(hidden_matrix_params + gate_params, lr=0.03, momentum=0.95, beta2=0.95, weight_decay=1.2)
+optimizer2 = NorMuon(hidden_matrix_params + gate_params, lr=0.10, momentum=0.95, beta2=0.95, weight_decay=1.2)
 # optimizer2 = NorMuon(hidden_matrix_params + gate_params, lr=0.1, momentum=0.95, beta2=0.95, weight_decay=1.2)
 optimizers = [optimizer1, optimizer2]
 for opt in optimizers:
@@ -1450,7 +1451,8 @@ def step_optimizers(step: int, optimizers, model):
 
     # on even steps, only step Muon params
     # on odd steps, step all params
-    if step%2==0:
+    # if step%2==0:
+    if step%8==0:
         optimizers[1].step()
         optimizers[1].zero_grad(set_to_none=True)
     else:
